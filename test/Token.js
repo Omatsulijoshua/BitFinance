30f9b1ae0d5c4ef2fd3dcc168dd415b8e4e8d59e
Omatsulijoshua <joshuaomatsuli01@gmail.comm.com>
2026-05-22 22:28:17 +0100

    3. Testing Cotracts

---
+++ test/Token.js
@@ -0,0 +1,43 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), "ether")
+}
+
+describe("Token", () => {
+	let token
+
+	beforeEach(async() => {
+		const Token = await ethers.getContractFactory("Token")
+		token = await Token.deploy("CFY Token", "CFYT", "21000000" )
+	})
+
+	describe("Deployment", () => {
+		const name = "CFY Token";
+		const symbol = "CFYT";
+		const decimals = "18";
+		const totalSupply = "21000000";
+
+
+		it("has correct name", async () => {
+			expect(await token.name()).to.equal(name)
+		})
+		it("has correct symbol", async () => {
+			expect(await token.symbol()).to.equal(symbol)
+		})
+
+		it("has correct decimals", async () => {
+			expect(await token.decimals()).to.equal(18)
+		})
+
+		it("has correct total supply", async () => {
+			expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+		})
+	})
+
+	
+
+
+
+})
