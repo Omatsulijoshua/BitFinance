09c73462ab42139e3be9b6d5e7337833d09e4927
Omatsulijoshua <joshuaomatsuli01@gmail.comm.com>
2026-05-23 04:49:13 +0100

    fix: normalize line endings

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,43 +9,43 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
-	// Track balances
-	mapping(address => uint256) public balanceOf;
+	//Track Balance
+	mapping (address => uint256) public balanceOf;
+	
 
 	event Transfer(
 		address indexed from,
 		address indexed to,
 		uint256 value
-	);
+		);
 
 	constructor(
-		string memory _name,
-		string memory _symbol,
-		uint256 _totalSupply
-	) {
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalSupply) 
+	{
 		name = _name;
 		symbol = _symbol;
-
-		totalSupply = _totalSupply * (10 ** decimals);
-
+		totalSupply = _totalSupply * (10**decimals); //21,000,000 x 10^18;
 		balanceOf[msg.sender] = totalSupply;
 	}
 
-	function transfer(address _to, uint256 _value)
-		public
-		returns (bool success)
-	{
-		require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-		require(_to != address(0), "Invalid recipient");
+	function transfer(address _to, uint256 _value) 
 
-		balanceOf[msg.sender] =
-			balanceOf[msg.sender] - _value;
+	public 
 
-		balanceOf[_to] =
-			balanceOf[_to] + _value;
+	returns (bool success) 
+	{   
+		require(balanceOf[msg.sender] >= _value);
+		require(_to != address(0));
 
+		balanceOf[msg.sender] =balanceOf[msg.sender] - _value;
+
+		balanceOf[_to] = balanceOf[_to] + _value;
 		emit Transfer(msg.sender, _to, _value);
-
 		return true;
 	}
-}
+	
+} 
+
+
